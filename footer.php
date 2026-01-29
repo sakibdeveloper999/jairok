e147ec36dbbd90e53e165bc102a4440a279d01d0
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-29 13:03:03 +0600

    Refactor footer, header, and index files to simplify structure and update content
    - Updated footer.php to replace existing content with a simple footer element.
    - Modified header.php to streamline the header structure and update the heading.
    - Refactored index.php to remove unnecessary content and add a main section with a heading.

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
-<div class="border-t border-gray-300 mt-4 py-4">
-  <div class="max-w-4xl px-4 mx-auto text-xs text-gray-400">Your footer content here.</div>
-</div>
-
-<?php wp_footer(); ?>
+    <footer>
+        <h1>it's footer</h1>
+    </footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html <?php language_attributes( ); ?> >
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
-    <div class="bg-gray-300">
-      <div class="max-w-4xl mx-auto mb-5 px-4">
-        <h1 class="text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-blue-500">Welcome To Our Header!</a></h1>
-      </div>
-    </div>
+</head>
+<body <?php body_class(); ?> >
+    <header>
+        <h1>it's header</h1>
+    </header>
